3b6a755011edcbb402cca1b2151df8ae443039b9
mirza-sync <mirzasyahmi123@gmail.com>
2023-02-18 15:57:46 +0800

    Display categories from db to blade

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // Route:view is to return static view (blade.php file)
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
